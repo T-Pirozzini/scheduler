64d53727db072acd6a5d041f3338b424f252c8d0
Travis Pirozzini <tpirozzini@gmail.com>
2022-03-23 14:30:14 -0700

    add InterviewList

--- src/components/InterviewerList.jsx
+++ src/components/InterviewerList.jsx
@@ -1,11 +1,25 @@
 import React from "react";
-// import classNames from "classnames";
-// import "components/InterviewerListItem.scss";
+import "components/InterviewerList.scss";
+import InterviewerListItem from "components/InterviewerListItem";
 
 export default function InterviewerList(props) {
-  const {interviewers, setInterviewer, interviewer} = props;
+  const { interviewers, setInterviewer, interviewer } = props;
 
-  return {
-    
-  }
+  const interviewerList = interviewers.map((currentInterviewer) => {
+    return (
+      <InterviewerListItem
+        key={currentInterviewer.id}
+        name={currentInterviewer.name}
+        avatar={currentInterviewer.avatar}
+        selected={currentInterviewer.id === interviewer}
+        setInterviewer={() => setInterviewer(currentInterviewer.id)}
+      />
+    );
+  });
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{interviewerList}</ul>
+    </section>
+  )
 }
