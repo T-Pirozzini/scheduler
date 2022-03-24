9829aff3f7f370e35a2c369322ce4913ab61d23a
Travis Pirozzini <tpirozzini@gmail.com>
2022-03-23 20:51:15 -0700

    completed display of appointments

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import "components/Appointment/styles.scss";
-import Header from "components/Appointment/Header"
-import Show from "components/Appointment/Show";
-import Empty from "components/Appointment/Empty";
+import Header from "./Header"
+import Show from "./Show";
+import Empty from "./Empty";
 
 export default function Appointment(props) {
   
   return (
     <article className="appointment">
-      <Header time={props.time}/>
+      <Header time={props.time} />
       {props.interview ? <Show student={props.interview.student} interview={props.interview.interviewer} />
       : <Empty />}
     </article>
